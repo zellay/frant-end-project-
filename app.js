b2627dd0d88e28c6726de3c50fc2801ade68b89f
zellay <zellitayman2004@gmail.com>
2024-12-18 16:24:32 +0100

    my first project
    this is my first front end development project using just html css and little bitt of js and it basic hase some animation in it

---
+++ app.js
@@ -0,0 +1,8 @@
+function showsidbar() {
+  const sidbar = document.querySelector(".a");
+  sidbar.style.display = "none";
+}
+function hidebar() {
+  const dbar = document.querySelector(".a");
+  dbar.style.display = "flex";
+}
